80ffeb7a42e43822f02f5504d23fd14f813d52a6
Nikhil Dubey <ndnikki1998@gmail.com>
2022-06-24 12:56:11 +0530

    Add files via upload

---
+++ MenuItemListing/MenuItem.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MenuItemListing
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool FreeDelivery { get; set; }
+        public int Price { get; set; }
+        public DateTime DateOfLaunch { get; set; }
+        public bool Active { get; set; }
+    }
+}
